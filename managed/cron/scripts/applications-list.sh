0878f7006647e1de6cfbf306a9dbcadba099a347
Ștefan Istrate <stefan.istrate@gmail.com>
2022-08-19 20:07:02 +0100

    Track macOS applications.

---
+++ managed/cron/scripts/applications-list.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if [[ ! -f /Applications ]]; then
+  exit
+fi
+
+REPO_DIR=$( mktemp -d )
+cd $REPO_DIR
+
+git clone --depth 1 https://github.com/stefanistrate/dotfiles.git .
+
+ls -1 /Applications > managed/cron/data/applications-list.txt
+
+git add managed/cron/data/applications-list.txt
+git commit -m "Sync macOS applications list."
+git push
