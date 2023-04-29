46c2d1bf7317c83ae8d0ffe42d4c5df49e9e9122
Ștefan Istrate <stefan.istrate@gmail.com>
2023-04-29 16:57:28 +0100

    Mail from cron only on error.

--- managed/cron/sync_homebrew_packages.sh
+++ managed/cron/sync_homebrew_packages.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+set -e
+
 PATH="$HOME/bin:$HOME/opt/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
 
 if [[ ! -x "$(command -v brew)" ]]; then
-  exit
+  echo "Missing brew command!" >&2
+  exit 1
 fi
 
 REPO_DIR=$(mktemp -d)
@@ -14,5 +17,5 @@ git clone --depth 1 git@github.com:stefanistrate/dotfiles.git .
 brew list --version >data/homebrew_packages.txt
 
 git add data/homebrew_packages.txt
-git commit -m "Sync Homebrew packages."
+git commit -m "Sync Homebrew packages." || exit 0
 git push
--- managed/cron/sync_macos_applications.sh
+++ managed/cron/sync_macos_applications.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+set -e
+
 PATH="$HOME/bin:$HOME/opt/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
 
 if [[ ! -d /Applications ]]; then
-  exit
+  echo "Missing /Applications directory!" >&2
+  exit 1
 fi
 
 REPO_DIR=$(mktemp -d)
@@ -14,5 +17,5 @@ git clone --depth 1 git@github.com:stefanistrate/dotfiles.git .
 ls -1 /Applications >data/macos_applications.txt
 
 git add data/macos_applications.txt
-git commit -m "Sync macOS applications."
+git commit -m "Sync macOS applications." || exit 0
 git push
