04319b41c9890dc3526d90824ab6cd8a00c4a2c4
Ștefan Istrate <stefan.istrate@gmail.com>
2023-08-17 09:43:04 +0100

    Snapshot macOS fonts

---
+++ tools/cron/snapshot_macos_fonts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+PATH="$HOME/bin:$HOME/opt/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
+
+REPO_DIR=$(mktemp -d)
+cd $REPO_DIR
+
+git clone --depth 1 git@github.com:stefanistrate/dotfiles.git .
+
+find /System/Library/Fonts/* 2> /dev/null | sort > snapshots/macos_fonts.txt
+find ~/Library/Fonts/* 2> /dev/null | sort >> snapshots/macos_fonts.txt
+
+git add snapshots/macos_fonts.txt
+git commit -m "Save current list of macOS fonts." || exit 0
+git push
